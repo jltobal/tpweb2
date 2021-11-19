939d4f1b713859765b70bc423c9a7608fbf18cf1
Sebastian Rosende Bricka <peperosende1515@gmail.com>
2021-11-18 22:25:55 -0300

    Actualizo db

--- db/db_impresoras_old.sql
+++ db/db_impresoras_old.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-11-2021 a las 02:25:09
--- Versión del servidor: 10.4.19-MariaDB
--- Versión de PHP: 8.0.6
+-- Tiempo de generación: 15-10-2021 a las 00:10:30
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,19 +31,27 @@ CREATE TABLE `impresoras` (
   `id_impresora` int(11) NOT NULL,
   `modelo` varchar(50) NOT NULL,
   `marca` varchar(50) NOT NULL,
-  `descripcion` varchar(100) NOT NULL,
-  `id_metodo_fk` int(11) NOT NULL,
-  `comentarios` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`comentarios`))
+  `descripcion` tinytext NOT NULL,
+  `id_metodo_fk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
 --
 -- Volcado de datos para la tabla `impresoras`
 --
 
-INSERT INTO `impresoras` (`id_impresora`, `modelo`, `marca`, `descripcion`, `id_metodo_fk`, `comentarios`) VALUES
-(3, 'LX300', 'Epson', 'Solo imprime en blanco y negro.', 2, '0'),
-(40, 'M651', 'Epson', 'Imprime color, escanea. ', 1, '0'),
-(41, 'M607', 'HP test 2', 'test', 1, '0');
+INSERT INTO `impresoras` (`id_impresora`, `modelo`, `marca`, `descripcion`, `id_metodo_fk`) VALUES
+(1, 'M650', 'HP', 'Multifunción, monocromática, escanea, panel visor, etc.', 1),
+(2, 'HT500', 'Metadatos', 'Impresora laser experimental', 5),
+(3, 'LX300', 'Epson', 'Solo imprime en blanco y negro.', 3),
+(4, 'FX890', 'Epson', 'Blanco y Negro. Velocidad de hasta 680 cps a 12 cp', 3),
+(5, 'Ender-3', 'Creality', 'Impresora FDM', 5),
+(6, 'Magna', 'Hellbot', 'Impresora FDM', 5),
+(7, 'TX200', 'Epson', 'Impresión a color. Contiene 4 cartuchos. Escanear.', 2),
+(8, 'P-HAS-180', 'Hasar', 'Impresora Termica Hasar Has 180 Usb Rs232 Tickets Comandera', 4),
+(9, 'Prusai3', 'PrusaResearch', 'Impresora FDM', 5),
+(10, 'Ender-5', 'Creality', 'Impresora FDM', 5),
+(11, 'Alpha 2r', 'TSC', 'Impresora térmica portátil de Tickets.', 7),
+(12, '6800', 'Kodak', 'Térmica fotográfica', 8);
 
 -- --------------------------------------------------------
 
@@ -53,7 +61,7 @@ INSERT INTO `impresoras` (`id_impresora`, `modelo`, `marca`, `descripcion`, `id_
 
 CREATE TABLE `metodos` (
   `id_metodo` int(11) NOT NULL,
-  `metodo` varchar(100) NOT NULL
+  `metodo` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
 --
@@ -63,28 +71,12 @@ CREATE TABLE `metodos` (
 INSERT INTO `metodos` (`id_metodo`, `metodo`) VALUES
 (1, 'Laser'),
 (2, 'Tinta'),
-(3, 'Matriz_de_punto'),
-(81, 'metodo nuevo'),
-(82, 'metodo nuevo 2');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `roles`
---
-
-CREATE TABLE `roles` (
-  `id_rol` int(11) NOT NULL,
-  `rol` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf32;
-
---
--- Volcado de datos para la tabla `roles`
---
-
-INSERT INTO `roles` (`id_rol`, `rol`) VALUES
-(1, 'administrador'),
-(2, 'userBasic');
+(3, 'Matriz de punto'),
+(4, 'Térmica'),
+(5, '3D'),
+(6, 'Experimental'),
+(7, 'Térmica portátil'),
+(8, 'Térmica Color');
 
 -- --------------------------------------------------------
 
@@ -95,18 +87,19 @@ INSERT INTO `roles` (`id_rol`, `rol`) VALUES
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `email` varchar(200) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `id_rol_fk` int(11) NOT NULL
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `email`, `password`, `id_rol_fk`) VALUES
-(55, 'user@test', '$2y$10$7OfozF0Vvy82QOY9LDcuJunhgeejsUVOyd9/3pgWmVvI6TMeZT9h6', 2),
-(64, 'admin@tudai', '$2y$10$fd/IcnY6yxDlFpMIczP4CeGK7fldtaz0OzoU6G1usERKytuqnGUp.', 1),
-(65, 'yo@vos', '$2y$10$Gv0Q4VrK.icWifmP2Tdr.uZaRHDpLwyxxTlEJXVhcdV2PlAxKUhQC', 2);
+INSERT INTO `usuarios` (`id`, `email`, `password`) VALUES
+(32, 'jose@', '$2y$10$ct3bGnZAvX6zJ0ay89gmdeQxlkHGQ8.JhXl2M4WSNnw'),
+(35, 'seba@', '$2y$10$VZ/0K2v9W6ZnrVbmktc8.O0mSS52WeaixDSsdh7gfwjMR.7.Hdkvm'),
+(37, 'jose@4', '$2y$10$7Yv6yeRvBhKB2idH1DqnY.EPZWIOeRTfIRUB6klt5YqUQtbZSAquS'),
+(38, 'sacoa@', '$2y$10$Az4FSwsDEaSPDtnzM58PkuCsXaK0oC.YrCYpIg2DGLm1jpvYPJLD6'),
+(39, 'admin@', '$2y$10$GGrdylSlxAJN/PZiClwfbeWHx/LpAcE3iogDF2HdLDrYOqQ49O62W');
 
 --
 -- Índices para tablas volcadas
@@ -117,7 +110,7 @@ INSERT INTO `usuarios` (`id`, `email`, `password`, `id_rol_fk`) VALUES
 --
 ALTER TABLE `impresoras`
   ADD PRIMARY KEY (`id_impresora`),
-  ADD KEY `id_metodo_fk` (`id_metodo_fk`);
+  ADD KEY `id_modelo_fk` (`id_metodo_fk`);
 
 --
 -- Indices de la tabla `metodos`
@@ -126,17 +119,10 @@ ALTER TABLE `metodos`
   ADD PRIMARY KEY (`id_metodo`);
 
 --
--- Indices de la tabla `roles`
---
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`id_rol`);
-
---
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_rol_fk` (`id_rol_fk`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -146,25 +132,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `impresoras`
 --
 ALTER TABLE `impresoras`
-  MODIFY `id_impresora` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id_impresora` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT de la tabla `metodos`
 --
 ALTER TABLE `metodos`
-  MODIFY `id_metodo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
-
---
--- AUTO_INCREMENT de la tabla `roles`
---
-ALTER TABLE `roles`
-  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_metodo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- Restricciones para tablas volcadas
@@ -175,12 +155,6 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `impresoras`
   ADD CONSTRAINT `impresoras_ibfk_1` FOREIGN KEY (`id_metodo_fk`) REFERENCES `metodos` (`id_metodo`);
-
---
--- Filtros para la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_rol_fk`) REFERENCES `roles` (`id_rol`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
